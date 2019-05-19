7a3e5f3ebca0207d2609326bec2995603bc75cfd
Claudio Ippolito <ippolitop@gmail.com>
2019-05-19 10:19:06 +0200

    Instantiated player and enemies

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ const Enemy = function Enemy(posX, posY, vel) {
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -23,10 +23,10 @@ Enemy.prototype.update = function(dt) {
     // thus, we are incrementing their location by each enemy
     // assigned dt constant they got
     this.posX += this.vel * dt;
-    
+
     // Checking if the enemies are at the canvas border, if so
     // reset their position and very the enemy speed
-    if (this.posX > 505 ) {
+    if (this.posX > 505) {
         this.posX = -50;
         let changeVel = Math.floor(Math.random() * 2);
         this.vel = changeVel;
@@ -34,53 +34,58 @@ Enemy.prototype.update = function(dt) {
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+Enemy.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.posX, this.posY);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-const Player = function Player(){
-    this.posX = 303;
+const Player = function Player() {
+    this.posX = 202;
     this.posY = 404;
-    this.upDown = 101;
-    this.leftRight = 83;
-    this.sprite = 'images/char-cat-girl.png'
+    this.horMove = 101;
+    this.verMove = 83;
+    this.sprite = 'images/char-boy.png';
 };
 
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.posX, this.posY);
 };
 
-Player.prototype.handleInput = function(whereTo) {
-    switch (whereTo){
+Player.prototype.handleInput = function (whereTo) {
+    switch (whereTo) {
         case 'up':
-        // move ...
-        break;
+            // move ...
+            break;
 
         case 'down':
-        // move ...
-        break;
+            // move ...
+            break;
 
         case 'left':
-        // move ...
-        break;
+            // move ...
+            break;
 
         case 'right':
-        // move ...
-        break;
+            // move ...
+            break;
     }
 };
 // Now instantiate your objects.
+let bug1 = new Enemy(0, 60 + 80 * 0, 20);
+let bug2 = new Enemy(0, 60 + 80 * 1, 20);
+let bug3 = new Enemy(0, 60 + 80 * 2, 20);
+
 // Place all enemy objects in an array called allEnemies
+window.allEnemies = [bug1, bug2, bug3];
+
 // Place the player object in a variable called player
-
-
+window.player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
